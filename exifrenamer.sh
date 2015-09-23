f1019bb2d08029ee052c6950f32b0cd6e81a9e1c
adelatour11 <adelatour11@yahoo.fr>
2015-09-23 15:52:41 +0200

    Update exifrenamer.sh

--- exifrenamer.sh
+++ exifrenamer.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+
+#get all the jpg files of the current folder
 for f in *.jpg
+
+# get the date in the exif data for each photo
 do
-test=$(exiv2 -g Exif.Image.DateTime -Pv $f)
-year=$(echo ${test::-14} | sed 's/\://g' | sed 's/\ //g')
-month=$(echo ${test:4:-12} | sed 's/\://g' | sed 's/\ //g')
-day=$(echo ${test:7:-8} | sed 's/\://g' | sed 's/\ //g')
+timestamp=$(exiv2 -g Exif.Image.DateTime -Pv $f)
+
+# from the timestamp, get the year, month and day seperately
+year=$(echo ${timestamp::-14} | sed 's/\://g' | sed 's/\ //g')
+month=$(echo ${timestamp:4:-12} | sed 's/\://g' | sed 's/\ //g')
+day=$(echo ${timestamp:7:-8} | sed 's/\://g' | sed 's/\ //g')
+
+# get the full date in order 
 fulldate=$(echo $year-$month-$day)
+
+# get the month name in letters
 monthinletters=$(date -d $fulldate '+%B')
 monthinletterscap=${monthinletters^}
+
+# set the destination folder
 path=$(echo $year/$monthinletterscap-$year/$day-$monthinletterscap-$year)
+
+# create the folders
 mkdir -p $path
+
+# rename the files
 exiv2 -r'%y%m%d-%H%M%S' -F rename $f
+
+# move the files to the destination folder 
 mv $f $path/
 done
